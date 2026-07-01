41c6dbf782ab50d59657b8b02cdc94d1fe147c3c
Bissash <bissash05@gmail.com>
2026-07-01 16:48:11 +0200

    idk man

--- Package.swift
+++ Package.swift
@@ -1,24 +1,13 @@
-// swift-tools-version: 6.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "GerneAppSharedDTO",
+    platforms: [.iOS(.v16), .macOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "GerneAppSharedDTO",
-            targets: ["GerneAppSharedDTO"]
-        ),
+        .library(name: "GerneAppSharedDTO", targets: ["GerneAppSharedDTO"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "GerneAppSharedDTO", path: "Sources/GerneAppSharedDTO"
-        ),
-
-    ],
-    swiftLanguageModes: [.v6]
+        .target(name: "GerneAppSharedDTO", path: "Sources/GerneAppSharedDTO"),
+    ]
 )
